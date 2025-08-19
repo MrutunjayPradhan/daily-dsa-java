b2b53887ffdc701e09c65e8614ecb6c5b7277eb3
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-08-19 21:54:59 +0530

    Solved leetcode 2348

---
+++ Daily/2025/08_August/Day19_2348_Solution.java
@@ -0,0 +1,17 @@
+public class Day19_2348_Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long count = 0;
+        long currentZeros = 0;
+        
+        for (int num : nums) {
+            if (num == 0) {
+                currentZeros++;
+                count += currentZeros;
+            } else {
+                currentZeros = 0;
+            }
+        }
+        
+        return count;
+    }
+}
