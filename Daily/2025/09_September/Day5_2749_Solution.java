4c2f2cc3c075dd0241b1570b1508fba403da2f8c
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-09-05 22:21:58 +0530

    feet: Solved Leetcode 2749

---
+++ Daily/2025/09_September/Day5_2749_Solution.java
@@ -0,0 +1,16 @@
+public class Day5_2749_Solution {
+
+    public int makeTheIntegerZero(int num1, int num2) {
+        int k = 1;
+        while (true) {
+            long x = num1 - (long) num2 * k;
+            if (x < k) {
+                return -1;
+            }
+            if (k >= Long.bitCount(x)) {
+                return k;
+            }
+            k++;
+        }
+    }
+}
