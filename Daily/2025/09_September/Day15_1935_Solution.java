a698f6bc46bca73b73ca01b334cdd728fecc337e
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-09-15 20:39:58 +0530

    feet: Solved leetcode 1935

---
+++ Daily/2025/09_September/Day15_1935_Solution.java
@@ -0,0 +1,17 @@
+public class Day15_1935_Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        String[] words = text.split(" ");
+        int count = 0;
+        for (String word : words) {
+            boolean canType = true;
+            for (char c : brokenLetters.toCharArray()) {
+                if (word.indexOf(c) != -1) {
+                    canType = false;
+                    break;
+                }
+            }
+            if (canType) count++;
+        }
+        return count;
+    }
+}
