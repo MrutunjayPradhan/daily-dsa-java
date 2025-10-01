fda0967fd823f5ce7fda9be73116cc2adc580738
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-10-01 23:28:28 +0530

    feet: Solved leetcode 1518

---
+++ Daily/2025/10_October/Day1_1518_Solution.java
@@ -0,0 +1,18 @@
+public class Day1_1518_Solution {
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int consumedBottles = 0;
+
+        while (numBottles >= numExchange) {
+            // Consume numExchange full bottles.
+            consumedBottles += numExchange;
+            numBottles -= numExchange;
+
+            // Exchange them for one full bottle.
+            numBottles++;
+        }
+
+        // Consume the remaining numBottles (<numExchange).
+        return consumedBottles + numBottles;
+    }
+}
