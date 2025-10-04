197ca0576026ed9e639e4bbc6b3c0cbb1edf8fb5
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-10-04 22:57:15 +0530

    feet: Solved Leetcode 11

---
+++ Daily/2025/10_October/Day4_11_Solution.java
@@ -0,0 +1,18 @@
+public class Day4_11_Solution {
+
+    public int maxArea(int[] height) {
+       int left =0;
+       int right=height.length -1;
+       int maxWater = Integer.MIN_VALUE;
+       while(left <right){
+        int currWater = (right - left) * Math.min(height[left],height[right]);
+        maxWater = Math.max(maxWater,currWater);
+        if(height[left]>height[right]){
+            right--;
+        }else{
+            left++;
+        }
+       }
+       return maxWater;
+    }
+}
