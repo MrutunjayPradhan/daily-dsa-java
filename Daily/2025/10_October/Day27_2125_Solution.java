6941fe149b390b9abd49e6526e6b25ed588e45fd
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-10-28 21:12:14 +0530

    feet: Solved Leetcode 2125

---
+++ Daily/2025/10_October/Day27_2125_Solution.java
@@ -0,0 +1,19 @@
+public class Day27_2125_Solution {
+    public int numberOfBeams(String[] bank) {
+        int prev =0, ans =0;
+
+        for(String s: bank){
+            int count =0;
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)=='1'){
+                    count++;
+                }
+            }
+            if(count >0){
+                ans +=prev*count;
+                prev = count;
+            }
+        }
+        return ans;
+    }
+}
