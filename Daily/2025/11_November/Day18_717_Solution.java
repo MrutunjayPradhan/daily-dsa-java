9401e2f6ac3af09ef113c326f70706d97e5494d2
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-11-18 21:32:54 +0530

    feet: Solved leetcode 717

---
+++ Daily/2025/11_November/Day18_717_Solution.java
@@ -0,0 +1,10 @@
+public class Day18_717_Solution {
+
+    public boolean isOneBitCharacter(int[] bits) {
+        int i = bits.length - 2;
+        while (i >= 0 && bits[i] > 0) {
+            i--;
+        }
+        return (bits.length - i) % 2 == 0;
+    }
+}
