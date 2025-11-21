9f7f51630d82ed39880ae254a0f4af650a8ffc2f
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-11-21 23:17:56 +0530

    feet: Solved Leetcode 1930

---
+++ Daily/2025/11_November/Day21_1930_Solution.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day21_1930_Solution {
+     public int countPalindromicSubsequence(String s) {
+        Set<Character> letters = new HashSet();
+        for(Character c:s.toCharArray()){
+            letters.add(c);
+        }
+        int ans =0;
+        for(Character letter:letters){
+            int i=-1;
+            int j=0;
+            for(int k=0;k<s.length();k++){
+                if(s.charAt(k)==letter){
+                    if(i==-1){
+                        i=k;
+                    }
+                    j=k;
+                }
+            }
+            Set<Character> between = new HashSet();
+            for(int k=i+1;k<j;k++){
+                between.add(s.charAt(k));
+            }
+            ans += between.size();
+        }
+        return ans;
+    }
+}
