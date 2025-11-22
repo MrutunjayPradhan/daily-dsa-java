fc2b4e3cab197d1bdd73092c6f362c45a33ba526
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-11-22 09:15:23 +0530

    feet: Solved leetcode 3190

---
+++ Daily/2025/11_November/Day22_3190_Solution.java
@@ -0,0 +1,10 @@
+public class Day22_3190_Solution {
+       public int minimumOperations(int[] nums) {
+        int sum =0;
+        for(int x : nums){
+            int rem = x%3;
+            sum += Math.min(rem,3-rem);
+        }
+        return sum;
+    }
+}
