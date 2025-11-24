7f2ef24b7558c625b50c54114ee0f8c803d455c2
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-11-24 22:54:55 +0530

    feet: Solved Leetcode 1018

---
+++ Daily/2025/11_November/Day24_1018_Solution.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+public class Day24_1018_Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> answer = new ArrayList<Boolean>();
+        int prefix =0;
+        int length = nums.length;
+
+        for(int i =0; i<length;i++){
+            prefix = ((prefix<<1)+nums[i]) %5;
+            answer.add(prefix==0);
+        }
+        return answer;
+    }
+}
