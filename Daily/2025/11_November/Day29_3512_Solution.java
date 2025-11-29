ac22855b558c673ac8af87b94c4a6eb6a0ed1497
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-11-29 21:50:35 +0530

    feet: Solved leetcode 3512

---
+++ Daily/2025/11_November/Day29_3512_Solution.java
@@ -0,0 +1,11 @@
+public class Day29_3512_Solution {
+
+    public int minOperations(int[] nums, int k) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum % k;
+    }
+
+}
