94742b05ab6b25b2187203a7eb9ca51ab5401362
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-12-05 19:28:22 +0530

    feet: Solved leetcode 3432

---
+++ Daily/2025/12_December/Day5_3432_Solution.java
@@ -0,0 +1,10 @@
+public class Day5_3432_Solution {
+      public int countPartitions(int[] nums) {
+        int totalSum =0;
+        for(int x : nums){
+            totalSum +=x;
+        }
+
+        return totalSum %2 ==0 ? nums.length -1 : 0;
+    }
+}
