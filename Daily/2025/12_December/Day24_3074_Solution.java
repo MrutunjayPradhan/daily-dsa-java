41428bfef3b4d46c2cf1d39fb02b189138e67276
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2025-12-25 18:21:15 +0530

    feet: Solved leetcode 3074

---
+++ Daily/2025/12_December/Day24_3074_Solution.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Day24_3074_Solution {
+
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int sum = 0;
+        for (int a : apple) {
+            sum += a;
+        }
+
+        Integer[] capArray = new Integer[capacity.length];
+        for (int i = 0; i < capacity.length; i++) {
+            capArray[i] = capacity[i];
+        }
+
+        Arrays.sort(capArray, Collections.reverseOrder());
+
+        int need = 0;
+        while (sum > 0) {
+            sum -= capArray[need];
+            need += 1;
+        }
+
+        return need;
+    }
+}
