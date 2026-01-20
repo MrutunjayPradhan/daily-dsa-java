73e52a7af577f98f4965300a52ee481425336561
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2026-01-20 21:53:08 +0530

    feet: Solved leetcode 3314

---
+++ Daily/2026/01_January/Day20_3314_Solution.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Day20_3314_Solution {
+
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = nums.get(i);
+            int res = -1;
+            int d = 1;
+            while ((x & d) != 0) {
+                res = x - d;
+                d <<= 1;
+            }
+            result[i] = res;
+        }
+        return result;
+    }
+}
