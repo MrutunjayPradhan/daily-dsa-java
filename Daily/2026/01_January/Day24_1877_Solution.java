04e04da8178fcba7d7fe485e35d4404dee376eab
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2026-01-24 23:37:24 +0530

    feet: Solved leetcode 1877

---
+++ Daily/2026/01_January/Day24_1877_Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Day24_1877_Solution {
+  public int minPairSum(int[] nums) {
+    Arrays.sort(nums);
+
+    int maxSum = 0;
+    for (int i = 0; i < nums.length / 2; i++) {
+      maxSum = Math.max(maxSum, nums[i] + nums[nums.length - 1 - i]);
+    }
+
+    return maxSum;
+
+  }
+}
